7f5ff95d85d010f600915bb5e50439b3e7c37b99
tzerozo <70289292+tzerozo@users.noreply.github.com>
2020-10-18 22:15:30 -0400

    Update GOP.V2.py
    Adds new classes and functions as needed.

--- GOP.V2.py
+++ GOP.V2.py
@@ -22,7 +22,7 @@ class Dice:
         self.number = random.randint(1,6)
         return self.number
 
-#####Base Class of Player.
+#####Base Class for Player.
 class Player:
     """ Attributes and Behaviors of players."""
     def __init__(self):
@@ -44,16 +44,36 @@ class Player:
             print("Please enter either 'r' or 'h'!")
             self.choose
 
+#####New this week.
 #####Subclass of Player class for computer player.
 class ComputerPlayer(Player):
     """A strategy for computer to hold or roll."""
     def ComputerDecision(self):
-      #(hold) <25 and 100-x (x = score)
+      #(hold)if <25 and 100-x (x = score)
+        if 25 < 100 - self.total_points:
+            hold_num = 25
+        else:
+            hold_num = 100 - self.total_points
 
+        if self.total_points < hold_num:
+            return 'r'
+        elif self.total_points >= hold_num:
+            return 'h'
+
+#####New this week.
+#####Factory Design Patterns.
 class PlayerFactory():
     """To instantiate the correct Player class."""
-    def selectplayer(self):
-
+    def __init__(self):
+        return None
+    def selectplayer(self, typeofplayer):
+        """To check the type of player."""
+        if typeofplayer == 'h':
+            return Player("Human Gamer")
+        elif typeofplayer == 'c':
+            return ComputerPlayer()
+        else:
+            print("Uh oh!!! something went wrong....")
 
 
 class Game:
@@ -113,18 +133,62 @@ class Game:
             elif self.currentplayer.hold == False and self.currentplayer.roll == True:
                 self.switch()
 
+#####New this week.
 #####Class for timed version of the game.At 1min, more points win.
-class TimedGameProxy():
+class TimedGameProxy(Game):
     """To have a timed(1 min)game so that winner can be announced."""
     def __init__(self):
+        self.start = time.time()
+        Game.__init__(self,player1,player2)
 
+    def timechecker(self):
+        """To check time."""
+        if time.time() - self.start >= 60:
+            if self.player1.total_points > self.player2.total_points:
+                print("Game Over, {} wins with {} points".format(self.player1.name,self.player1.total_points))
+            else:
+                print("Game Over, {} wins with {} points".format(self.player2.name,self.player2.total_points))
+        else:
+            sys.exit()
 
+    def next_player(self):
+        self.total_points = 0
+        if self.player1.score >= 100:
+            print("Gamer1 has won!!!")
+            sys.exit()
+        elif self.player2.score >= 100:
+            print("Gamer2 has won!!!")
+            sys.exit()
+        else:
+            if self.currentplayer == self.player1:
+                self.currentplayer = self.player2
+            elif self.currentplayer == self.player2:
+                self.currentplayer = self.player1
+        print(self.currentplayer.name, "Will take turn")
+        self.switch()
 
+    def switch(self):
+        """Taking turn of rolling"""
+        print("Gamer1's total_points: ", self.player1.score)
+        print("Gamer2's total_points: ", self.player2.score)
+        self.dice.roll_dice()
+        if self.dice.number == 1:
+            print("Sorry you rolled 1, we will forfeit your turn")
+            self.total_points = 0
+            self.next_player()
+        else:
+            self.total_points += self.dice.number
+            print(self.dice.number," is rolled, you will earn the points")
+            print("Your current score for this turn is ", self.total_points)
+            self.currentplayer.choose()
+            if self.currentplayer.hold == True and self.currentplayer.roll == False:
+                self.currentplayer.score += self.total_points
+                self.next_player()
+            elif self.currentplayer.hold == False and self.currentplayer.roll == True:
+                self.switch()
 
-
-        
-def main():
-    """To start the program/game"""
+#####To start a new game
+def GameofPig():
     begin = input("Are you ready? Enter: y or n: ")
     if begin == 'y':
         player1 = Player()
@@ -136,7 +200,21 @@ def main():
         sys.exit()
     else:
         print("You must enter 'y' or 'n'")
-        main()
+        GameofPig()
+    
+def main():
+    """To start the program/game"""
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--player1', help='Chooose type of player: human or computer')
+    parser.add_argument('--player2', help='Choose type of player: human or computer')
+    parser.add_argument('--timed', help='Game will last 1 minute')
+    args = parser.parse_args()
+
+    if args.timed:
+        timed = TimedGameProxy()
+    else:
+        regular = GameofPig()
+    
 
 if __name__ == '__main__':
     main()
